12ad6c2cdd3fb50ccda05c972fe161258f7845e4
Eliza Velasquez <4576666+elizagamedev@users.noreply.github.com>
2023-09-10 16:42:48 -0700

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     applicationId = "sh.eliza.textbender"
     minSdk = 26
     targetSdk = 33
-    versionCode = 3
-    versionName = "0.3.0"
+    versionCode = 4
+    versionName = "0.3.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
